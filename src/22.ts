a4dd3130297d301f0c4d501e2ea95883cefbe89e
nelsonsosa851 <nelsonsosa851@gmail.com>
2025-04-03 12:54:25 +0000

    Add 22.ts

---
+++ src/22.ts
@@ -0,0 +1,8 @@
+// My project name: SchoolProject
+
+// Import statements can be added here if needed.
+import { myFunction } from "./my-module";
+
+export function main() {
+  console.log("Hello World!");
+}
